246a2bf3d25c43c07a6e0e8f8982df294a40ed77
PanosAntoniadis <pantoniadis97@gmail.com>
2019-06-02 00:31:17 +0300

    Add function that connects to account and reads raw emails

---
+++ email-extraction/email_extraction.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def connect():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    return service
+
+def readEmails(service):
+    # Call the Gmail API
+    results = service.users().messages().list(userId='me',labelIds = ['INBOX']).execute()
+    messages = results.get('messages', [])
+
+    for message in messages:
+        msg = service.users().messages().get(userId='me', id=message['id']).execute()
+        print(msg['snippet'])
+
+
+if __name__ == '__main__':
+    service = connect()
+    readEmails(service)
