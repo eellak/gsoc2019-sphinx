1e0b57ce5ca53b4cc1288693535c925f1ea261a2
PanosAntoniadis <pantoniadis97@gmail.com>
2019-06-17 01:07:21 +0300

    Add kmeans method and find optimal number of clusters

--- email_processing/clustering.py
+++ email_processing/clustering.py
@@ -5,10 +5,19 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 import spacy
 
 
-def get_spacy_vec(emails):
+def get_spacy(emails):
+    '''Represent emails as vectors using the spacy Greek model.
+
+        Args:
+            emails: A list that contains the emails in string format.
+        Returns:
+            X: A list that contains the vectors of the emails.
+
+        '''
     nlp = spacy.load('el_core_news_md')
     X = []
     for email in emails:
@@ -18,23 +27,50 @@ def get_spacy_vec(emails):
 
 
 def get_tfidf(emails):
+    '''Represent emails as vectors using the tf-idf.
+
+        Args:
+            emails: A list that contains the emails in string format.
+        Returns:
+            X: A list that contains the tf-idf of the emails.
+
+        '''
     # Compute tf idf vectorizer of emails.
     tf = TfidfVectorizer()
-    # Or fit_transform together
     emails_fitted = tf.fit(emails)
     emails_transformed = emails_fitted.transform(emails)
+    # Or fit_transform together
     return emails_transformed
+
+
+def get_optimal_clusters(X):
+    '''Run k-means and search for the omptimal
+        number of clusters by using the Elbow Criterion.
+
+        Args:
+            X: A list that contains the vectors of the emails.
+        '''
+
+    sse = []
+    for k in range(1, len(X)):
+        clf = KMeans(n_clusters=k)
+        clf = clf.fit(X)
+        sse.append(clf.inertia_)
+
+    plt.plot(range(1, len(X)), sse, 'bx-')
+    plt.xlabel('k')
+    plt.ylabel('Sum_of_squared_error')
+    plt.title('Elbow Method For Optimal k')
+    plt.show()
 
 
 if __name__ == '__main__':
     # Get emails
     emails = get_emails('./texts/')
 
-    X1 = get_spacy_vec(emails)
-    X2 = get_tfidf(emails)
+    # Get representation of emails.
+    X_spacy = get_spacy(emails)
+    # X_tfidf = get_tfidf(emails)
 
-    n_clusters = 3
-    clf = KMeans(n_clusters=n_clusters, max_iter=100,
-                 init='k-means++', n_init=1)
-    labels = clf.fit_predict(X2)
-    print(labels)
+    # Find the optimal number of clusters
+    get_optimal_clusters(X_spacy)
