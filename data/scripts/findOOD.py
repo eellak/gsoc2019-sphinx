b4f885e756bf21c6bdcc1d64bc3d9f02495c0a41
PanosAntoniadis <pantoniadis97@gmail.com>
2019-06-22 23:49:42 +0300

    Fix minor bug and add print parameter

--- data/scripts/findOOD.py
+++ data/scripts/findOOD.py
@@ -35,12 +35,15 @@ if __name__ == '__main__':
     required.add_argument(
         '--input', help="Path of input transcription (should be in Sphinx format)", required=True)
     required.add_argument(
-        '--output', help="File to write the missing words", required=True)
+        '--output', help="Path of output file", required=True)
+    optional.add_argument(
+        '--print_missing', help="If True, prints missing words")
 
     args = parser.parse_args()
     input = args.input
     output = args.output
     dict = args.dict
+    print_missing = args.print_missing
 
     if not os.path.isfile(dict) or not os.path.isfile(input):
         sys.exit("Wrong arguments")
@@ -56,11 +59,12 @@ if __name__ == '__main__':
             words = line.split(' ')
             # Last word is the identity of the transcription, so
             # don't care about it.
-            print('Searching for transcription: ' + words[-1].split('\n'))
+            print('Searching for transcription: ' + words[-1].strip('\n'))
             for word in words[:len(words) - 1]:
                 if word:
                     if (not searchDic(word, dict)) and (word not in missing):
-                        print(word)
+                        if print_missing:
+                            print(word)
                         w.write(word + '\n')
                         missing.append(word)
 
