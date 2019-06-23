ff41d3bb4b4025b0c736bb7065c97f62cc933061
PanosAntoniadis <pantoniadis97@gmail.com>
2019-06-23 17:59:57 +0300

    Add parameters for creating mixed language models

--- email_clustering/cluster2lm.py
+++ email_clustering/cluster2lm.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import argparse
+import sys
 
 if __name__ == '__main__':
     # Create an argument parser
@@ -9,12 +10,20 @@ if __name__ == '__main__':
     ''')
 
     required = parser.add_argument_group('required arguments')
+    optional = parser.add_argument_group('optional arguments')
     required.add_argument(
         '--input', help="Input directory that contains the clusters", required=True)
+    optional.add_argument(
+        '--mix', help="If set, create a merged lm with the given model")
 
     args = parser.parse_args()
     input = args.input
+    mix = args.mix
     for cluster in os.listdir(input):
         cluster_path = os.path.join(input, cluster)
-        if subprocess.call(['ngram-count -kndiscount -interpolate -text ' + os.path.join(cluster_path, 'corpus') + ' -wbdiscount1 -wbdiscount2 -wbdiscount3 -lm ' + os.path.join(cluster_path, 'model.lm')], shell=True):
-            sys.exit('Error in subprocess')
+        if os.path.isdir(cluster_path):
+            if subprocess.call(['ngram-count -kndiscount -interpolate -text ' + os.path.join(cluster_path, 'corpus') + ' -wbdiscount1 -wbdiscount2 -wbdiscount3 -lm ' + os.path.join(cluster_path, 'model.lm')], shell=True):
+                sys.exit('Error in subprocess')
+            if mix is not None:
+                if subprocess.call(['ngram -lm ' + mix + ' -mix-lm ' + os.path.join(cluster_path, 'model.lm') + ' -lambda 0.5 -write-lm ' + os.path.join(cluster_path, 'merged.lm')], shell=True):
+                    sys.exit('Error in subprocess')
