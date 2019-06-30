a49fcb43648ddb1b1f59c9e12f24e4dd0ad4ce0e
PanosAntoniadis <pantoniadis97@gmail.com>
2019-06-30 14:51:20 +0300

    Add functions for finding closest pos string

--- post_processing/helper.py
+++ post_processing/helper.py
@@ -1,4 +1,39 @@
 import os
+import editdistance
+
+
+def pos2str(pos):
+    '''Each POS tag of a sentence is in form [(word1, pos1), ..., (wordN, posN)]. This function
+        converts this form into two strings that contain the word and the pos sequence.
+
+        Args:
+            pos: A list of sentences in above form.
+        Returns:
+            pos_str: A list of strings that contains each sequence of pos tags separated by space.
+            text_str: A list of strings that contains each sentence.
+        '''
+    pos_str = []
+    text_str = []
+    for sentence in pos:
+        text_str.append(' '.join(elem[0] for elem in sentence))
+        pos_str.append(' '.join(elem[1] for elem in sentence))
+    return pos_str, text_str
+
+
+def closest_string(x, corpus):
+    '''Computes the closest string of x based on the Levenshtein dictance.
+
+        Args:
+            x: The string from which the closest string we search.
+            corpus: A list of possible closest strings.
+        Returns:
+            closest: The closest string from x.
+            dist: The dictance from the closest string.
+        '''
+    distances = [editdistance.eval(x, elem) for elem in corpus]
+    min_dist = min(distances)
+    closest = corpus[distances.index(min_dist)]
+    return closest, min_dist
 
 
 def get_emails(dir):
