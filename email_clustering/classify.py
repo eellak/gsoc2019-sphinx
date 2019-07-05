b9596b6b0a8eb0b75a7507c8c94c648d854c708c
PanosAntoniadis <pantoniadis97@gmail.com>
2019-07-05 12:56:29 +0300

    Keep predicted labels in a dictionary instead of a list

--- email_clustering/classify.py
+++ email_clustering/classify.py
@@ -16,14 +16,20 @@ if __name__ == '__main__':
         '--input', help="Input transriptions (one per line)", required=True)
     required.add_argument(
         '--centers', help="Pickle file that contains the centers", required=True)
+    required.add_argument(
+        '--ids', help="Ids of the transcriptions", required=True)
+    required.add_argument(
+        '--n_clusters', help="Number of clusters", required=True, type=int)
     optional.add_argument(
-        '--has_id', help="If set, each email contains an id in the end", action='store_true')
+        '--has_id', help="If set, each email contains his id in the end", action='store_true')
     optional.add_argument(
         '--save', help="If set, save labels in pickle format", action='store_true')
 
     args = parser.parse_args()
     input = args.input
     centers_path = args.centers
+    ids_path = args.ids
+    n_clusters = args.n_clusters
     has_id = args.has_id
     save = args.save
 
@@ -31,15 +37,20 @@ if __name__ == '__main__':
     with open(centers_path, 'rb') as f:
         centers = pickle.load(f)
 
+    ids = []
+    with open(ids_path, 'r') as r:
+        for line in r:
+            ids.append(line.strip('\n'))
+
     # Get emails to classify
     emails = get_emails_from_transcription(input, has_id)
     # Represent them as vectors
     vectors = get_spacy(emails)
-    labels = []
-    for vec in vectors:
+
+    labels = {k: [] for k in range(n_clusters)}
+    for i, vec in enumerate(vectors):
         cluster = closest_cluster(centers, vec)
-        labels.append(cluster)
-        print(cluster)
+        labels[cluster].append(ids[i])
 
     # Save labels in pickle format
     if save:
