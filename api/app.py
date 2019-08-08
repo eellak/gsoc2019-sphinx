dac9f17d86d14663f925a830664134649b60d91d
PanosAntoniadis <pantoniadis97@gmail.com>
2019-08-09 01:55:25 +0300

    Add help scripts of clustering and initialize clustering endpoint

--- api/app.py
+++ api/app.py
@@ -11,11 +11,11 @@ from flask_login import (
     login_user,
     logout_user,
 )
-from user import User
 import requests
 import json
 import email
-from helper import get_body, get_charset, get_header, mime2str, process_text
+from preprocess_helper import get_body, get_charset, get_header, mime2str, process_text
+from clustering_helper import get_spacy, get_metrics, closest_point, extract_topn_from_vector, sort_coo
 from email.header import decode_header
 from email.iterators import typed_subpart_iterator
 import chardet
@@ -53,10 +53,9 @@ def getInfo():
     return jsonify(returned_data)
 
 
-@app.route("/messages", methods=["POST"])
+@app.route("/emails", methods=["POST"])
 def getMessages():
     data = request.form
-    #token = data['token']
     cookie = data['cookie']
     res = database.find_one('info', {'_id': cookie})
     token = res['token']
@@ -90,6 +89,69 @@ def getMessages():
         'messages', {'_id': cookie, 'messages': clean_messages})
     # Send user back to homepage
     return jsonify(clean_messages)
+
+
+@app.route("/clustering", methods=["POST"])
+def getClusters():
+    data = request.form
+    cookie = data['cookie']
+    # Get optional arguments
+    metric = data['metric']
+    n_clusters = data['n_clusters']
+    method = data['method']
+    min_cl = data['min_cl']
+    max_cl = data['max_cl']
+    sentence = data['sentence']
+
+    res = database.find_one('messages', {'_id': cookie})
+    messages_col = res['messages']
+    emails = []
+    for msg in messages_col:
+        emails.append(msg['proccesed_body'])
+
+    X = get_spacy(emails)
+    if n_clusters is None:
+        # Get metrics in different number of clusters (range [min_cl, max_cl]).
+        sse, silhouette = get_metrics(X, min_cl, max_cl)
+        if method == 'elbow':
+            n_clusters = find_knee(sse, min_cl)
+        else:
+            n_clusters = silhouette_analysis(silhouette, min_cl)
+    # Run k-means with given number of clusters.
+    labels, centers = run_kmeans(X, n_clusters)
+
+    # TODO: save clusters in db
+    # TODO: save centers in db
+
+    # TODO: save samples in db
+    # TODO: save keywords in db
+
+    if samples:
+        # Save the closest email in each center.
+        with open(os.path.join(output + 'samples'), 'w') as w:
+            for i in range(n_clusters):
+                w.write('Cluster ' + str(i) + '\n')
+                w.write(emails[closest_point(centers[i], X, metric)])
+                w.write('\n')
+
+    if keywords:
+        # We want to keep some representative words for each cluster
+        # in order to identify the topic it represents. So we take
+        # the words with the heighest tf-idf metric in each cluster.
+        cv = CountVectorizer(stop_words=STOP_WORDS)
+        tfidf = TfidfTransformer(smooth_idf=True, use_idf=True)
+        for i in range(n_clusters):
+            emails_cluster = [emails[j]
+                              for j in range(len(emails)) if labels[j] == i]
+            word_count_vector = cv.fit_transform(emails_cluster)
+            tfidf.fit(word_count_vector)
+            feature_names = cv.get_feature_names()
+            tf_idf_vector = tfidf.transform(
+                cv.transform(emails_cluster))
+            sorted_items = sort_coo(tf_idf_vector.tocoo())
+            keywords = extract_topn_from_vector(
+                feature_names, sorted_items, 5)
+    return '1'
 
 
 if __name__ == "__main__":
---
+++ api/clustering_helper.py
@@ -0,0 +1,214 @@
+import os
+import numpy as np
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
+import sys
+from stop_words import STOP_WORDS
+import re
+from gensim.models import Word2Vec
+from gensim.models.fasttext import FastText
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
+import scipy
+from sklearn.metrics.pairwise import cosine_similarity
+
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score, silhouette_samples
+from numpy import sqrt
+from kneed import KneeLocator
+from multiprocessing import cpu_count
+import logging
+
+
+def sort_coo(coo_matrix):
+    tuples = zip(coo_matrix.col, coo_matrix.data)
+    return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
+
+
+def extract_topn_from_vector(feature_names, sorted_items, topn=10):
+    ''' Get the feature names and tf-idf score of top n items'''
+
+    # use only topn items from vector
+    sorted_items = sorted_items[:topn]
+
+    score_vals = []
+    feature_vals = []
+
+    # word index and corresponding tf-idf score
+    for idx, score in sorted_items:
+
+        # keep track of feature name and its corresponding score
+        score_vals.append(round(score, 3))
+        feature_vals.append(feature_names[idx])
+
+    # create a tuples of feature,score
+    # results = zip(feature_vals,score_vals)
+    results = {}
+    for idx in range(len(feature_vals)):
+        results[feature_vals[idx]] = score_vals[idx]
+
+    return results
+
+
+def get_spacy(emails):
+    '''Represent emails as vectors using the spacy Greek model.
+
+        Args:
+            emails: A list that contains the emails in string format.
+        Returns:
+            X: A list that contains the vectors of the emails.
+
+        '''
+    nlp = spacy.load('el_core_news_md')
+    X = []
+    for email in emails:
+        doc = nlp(email)
+        X.append(doc.vector)
+    return X
+
+
+def cluster2text(out, n_clusters):
+    for i in range(n_clusters):
+        cluster_path = os.path.join(out, 'cluster_' + str(i))
+        email_path = os.path.join(out, 'cluster_' + str(i) + '/data')
+        with open(os.path.join(cluster_path, 'corpus'), 'w') as w:
+            for email in os.listdir(email_path):
+                with open(os.path.join(email_path, email), 'r') as r:
+                    w.write(r.read())
+                    w.write('\n')
+
+
+def closest_cluster(centers, point, metric):
+    '''Find the cluster that a point belongs.
+
+        Args:
+            centers: The coordinates of the centers of the clusters
+            point: The coordinates of the point (vector representation of a text)
+            metric: Metric to be used (euclidean or cosine)
+        Returns:
+            cluster: Closest cluster to the point.
+
+        '''
+    cluster_id = 0
+    if metric == 'euclidean':
+        distance = np.linalg.norm(centers[0] - point)
+        for i in range(1, len(centers)):
+            cur_distance = np.linalg.norm(centers[i] - point)
+            if cur_distance < distance:
+                distance = cur_distance
+                cluster_id = i
+    else:
+        distance = cosine_similarity([centers[0]], [point])[0][0]
+        for i in range(1, len(centers)):
+            cur_distance = cosine_similarity(
+                [centers[i]], [point])[0][0]
+            if cur_distance > distance:
+                distance = cur_distance
+                cluster_id = i
+    return cluster_id
+
+
+def closest_point(center, X, metric):
+    '''Find the point of X that is closer in center.
+
+        Args:
+            center: The coordinates of the center
+            point: The coordinates of the points (vector representation of emails)
+            metric: Metric to be used
+        Returns:
+            min_point: Index of the closest point to the cluster
+
+        '''
+    if metric == 'euclidean':
+        distance = np.linalg.norm(center - X[0])
+    else:
+        distance = cosine_similarity([center], [X[0]])[0][0]
+    min_point = 0
+    total = len(X)
+    for i in range(1, total):
+        if metric == 'euclidean':
+            cur_distance = np.linalg.norm(center - X[i])
+            if cur_distance < distance:
+                distance = cur_distance
+                min_point = i
+        else:
+            cur_distance = cosine_similarity([center], [X[i]])[0][0]
+            if cur_distance > distance:
+                distance = cur_distance
+                min_point = i
+    return min_point
+
+
+def get_metrics(X, plot, min_cl, max_cl):
+    '''Run k-means and keep sum of squared errors and silhouette coefficients
+        for each number of clusters.
+
+        Args:
+            X: A list that contains the vectors of the emails.
+            min_cl: Minimum number of clusters (at least 2).
+            max_cl: Maximum number of clusters (up to n_samples-1)
+        Returns:
+            sse: A list that contains the sum of squared erros per cluster.
+            silhouette: A list that contains the silhouette score per cluster.
+
+        '''
+
+    ks = range(min_cl, max_cl)
+    sse = []
+    silhouette = []
+    for k in ks:
+        clf = KMeans(n_clusters=k, n_jobs=cpu_count(),
+                     n_init=10 * cpu_count())
+        logging.info(clf)
+        clf = clf.fit(X)
+        sse.append(clf.inertia_)
+        silhouette.append(silhouette_score(X, clf.labels_))
+    return sse, silhouette
+
+
+def run_kmeans(X, n_clusters):
+    '''Run k-means algorithm for given number of clusters.
+
+        Args:
+            X: A list that contains the vectors.
+            n_clusters: number of clusters
+        Returns:
+            labels: Label of each vector.
+
+        '''
+    # Run k-means usign n_clusters
+    clf = KMeans(n_clusters=n_clusters, n_jobs=cpu_count(),
+                 n_init=10 * cpu_count())
+    labels = clf.fit_predict(X)
+    return labels, clf.cluster_centers_
+
+
+def find_knee(sse, min_cl):
+    '''Find optimal number of clusters using the elbow method. More info here: https://github.com/arvkevi/kneed
+
+        Args:
+            sse: A list that contains the sum of squared errors.
+            min_cl: Minimum number of clusters
+        Returns:
+            n_clusters: Optimal number of clusters
+
+        '''
+    k = range(min_cl, len(sse) + min_cl)
+    kneedle = KneeLocator(list(k), sse, curve='convex',
+                          direction='decreasing')
+    n_clusters = kneedle.knee
+    return n_clusters
+
+
+def silhouette_analysis(silhouette, min_cl):
+    '''Find optimal number of clusters using the silhouette method.
+
+        Args:
+            sse: A list that contains the silhouette scores.
+            min_cl: Minimum number of clusters
+        Returns:
+            n_clusters: Optimal number of clusters
+
+        '''
+    n_clusters = silhouette.index(max(silhouette))
+    return n_clusters + min_cl
