02f24503dc06375fb372333c23a8a0910b8bfd81
PanosAntoniadis <pantoniadis97@gmail.com>
2019-08-25 19:42:08 +0300

    Add comments in api

--- api/app.py
+++ api/app.py
@@ -8,8 +8,6 @@ from flask_cors import CORS, cross_origin
 import requests
 import json
 import email
-from preprocess_helper import get_body, get_charset, get_header, mime2str, process_text
-from clustering_helper import get_spacy, get_metrics, closest_point, extract_topn_from_vector, sort_coo, silhouette_analysis, find_knee, run_kmeans, save_clusters, cluster2text, closest_cluster
 from stop_words import STOP_WORDS
 from email.header import decode_header
 from email.iterators import typed_subpart_iterator
@@ -19,29 +17,28 @@ import base64
 from flask_cors import CORS
 import database
 import spacy
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 import subprocess
 import base64
 import urllib
 import numpy as np
-from dictation_helper import get_text_sphinx4, get_text_pocketsphinx
-from py4j.java_gateway import JavaGateway
 import random
 import shutil
-import arpa
-from nltk.util import ngrams
+from preprocess_helper import get_body, get_charset, get_header, mime2str, process_text
+from clustering_helper import get_spacy, get_metrics, closest_point, extract_topn_from_vector, sort_coo, silhouette_analysis, find_knee, run_kmeans, save_clusters, cluster2text, closest_cluster
+from dictation_helper import get_text_sphinx4, get_text_pocketsphinx
+from post_processing_helper import error_detector
 
 # Flask app setup
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 app.config['JSON_AS_ASCII'] = False
+
 # Enable CORS
 CORS(app)
 # Load spacy Greek model
 nlp = spacy.load('el_core_news_md')
-# Connect to a JVM with a gateway in order to communicate with Java cmuSphinx
-gateway = JavaGateway()
+
 # Get useful paths for defaul models.
 acousticPath = os.environ.get("general_ac")
 dictPath = os.environ.get("general_dict")
@@ -65,12 +62,11 @@ app.after_request(add_cors_headers)
 
 
 def getInfo(token):
-    # Send get request to gmail api.
+    # Send get request to gmail api to get the user info.
     info_endpoint = "https://www.googleapis.com/oauth2/v1/userinfo?alt=json"
     headers = {'Authorization': 'Bearer ' +
                token, 'Accept': 'application/json'}
     info_response = requests.get(info_endpoint, headers=headers)
-
     email = info_response.json()["email"]
     name = info_response.json()["given_name"]
     picture = info_response.json()["picture"]
@@ -78,71 +74,77 @@ def getInfo(token):
     return email, name, picture
 
 
-@app.route("/emails", methods=["POST"])
+@app.route('/emails', methods=['POST'])
 def getEmails():
     '''Endpoint that returns the sent emails of a Gmail user.
 
-        Args:
+        Body:
             cookie: Cookie of current user.
+            token: Authentication token from Gmail API.
+            keep: If true, keep current user;s data after log out.
         '''
+
     data = request.form
-    # Get authentication token of current user.
     token = data['token']
     cookie = data['cookie']
     keep = data['keep']
+    # Get user's info and save a new connection in the database.
     email_name, name, picture = getInfo(token)
-
     database.insert_one(
         'connections', {'_id': cookie, 'email_name': email_name, 'keep': keep}
     )
-    # Save user in database.
+
+    # If user's data exist, return the emails.
     res = database.find_one('users', {'_id': email_name})
     if res is not None:
         res = database.find_one(
             'messages', {'_id': email_name})
-        messages = res["messages"]
-        return jsonify(messages)
+        if res is not None:
+            messages = res['messages']
+            return jsonify(messages)
+        else:
+            # In case data are not sychronized correctly.
+            database.delete_one('users', {'_id': email_name})
 
     database.insert_one(
         'users', {'_id': email_name, 'name': name, 'picture': picture})
-    # Send get request in gmail api.
+    # Send get request in gmail api to get the user's emails.
     read_endpoint = "https://www.googleapis.com/gmail/v1/users/userId/messages"
     headers = {'Authorization': 'Bearer ' +
                token, 'Accept': 'application/json'}
     read_response = requests.get(read_endpoint, headers=headers, params={
                                  'userId': 'me', 'labelIds': ['SENT']})
-    messages = read_response.json().get("messages")
+    messages = read_response.json().get('messages')
     clean_messages = []
     for idx, message in enumerate(messages):
-        print(idx)
-        # Get message based in the id.0.5311486608012452
+        # Get message based in the id.
         get_endpoint = "https://www.googleapis.com/gmail/v1/users/userId/messages/id"
         get_response = requests.get(get_endpoint, headers=headers, params={
             'userId': 'me', 'id': message['id'], 'format': 'raw'})
         raw_msg = get_response.json().get("raw")
-        print(type(raw_msg))
         string_message = str(
             base64.urlsafe_b64decode(raw_msg), "ISO-8859-7")
         # Convert current message to mime format.
         mime_msg = email.message_from_string(string_message)
         # Convert current message from mime to string.
         body, msg_headers = mime2str(mime_msg)
+        # Clean current message
         proccesed_body = process_text(body)
         size = len(msg_headers)
         clean_messages.append(
             {'body': body, 'processed_body': proccesed_body, 'sender': (msg_headers[0] if size > 0 else " "), 'subject': (msg_headers[2] if size > 2 else " ")})
 
-    # Save user emails in database.
+    # Save user's emails in database and return them.
     database.insert_one(
         'messages', {'_id': email_name, 'messages': clean_messages})
     return jsonify(clean_messages)
 
 
-@app.route("/clustering", methods=["POST"])
+@app.route('/clustering', methods=['POST'])
 def getClusters():
     '''Endpoint that clusters the emails.
 
-        Args:
+        Body:
             cookie: Cookie of current user.
             metric: Metric to be used for closest point calculation.
             n_clusters: Number of clusters.
@@ -160,19 +162,21 @@ def getClusters():
     max_cl = int(data['max_cl'])
     level = data['level']
 
-    # Get current user
+    # Get current user.
     res = database.find_one('connections', {'_id': cookie})
     email_name = res['email_name']
 
-    # Get messages current user
+    # Get messages of current user.
     res = database.find_one('messages', {'_id': email_name})
     messages_col = res['messages']
+    # Keep them as sentences if asked to.
     emails = []
     for msg in messages_col:
         if level == "sentence":
             emails.extend(msg['processed_body'])
         else:
             emails.append(" ".join(msg['processed_body']))
+
     # Represent them as vectors.
     X = get_spacy(emails, nlp)
 
@@ -188,13 +192,12 @@ def getClusters():
     labels, centers = run_kmeans(X, n_clusters)
 
     # Save computed clusters in filesystem.
-    save_clusters(emails, labels, os.path.join(email_name, 'clusters'))
     out = os.path.join('./data', os.path.join(email_name, 'clusters'))
+    save_clusters(emails, labels, os.path.join(email_name, 'clusters'))
     cluster2text(out, n_clusters)
 
-    # Get a sample from each cluster.
+    # Get a sample for each cluster.
     samples = []
-    # Save the closest email in each center.
     for i in range(n_clusters):
         samples.append(emails[closest_point(centers[i], X, metric)])
 
@@ -214,11 +217,12 @@ def getClusters():
             cv.transform(emails_cluster))
         sorted_items = sort_coo(tf_idf_vector.tocoo())
         keywords = extract_topn_from_vector(
-            feature_names, sorted_items, 5)
+            feature_names, sorted_items, 10)
         keywords_total.append(keywords)
 
+    # Delete previous user's clustering.
     database.delete_one('clusters', {'_id': email_name})
-    # Insert in database.
+    # Insert computed clusters in database.
     database.insert_one('clusters', {'_id': email_name, 'centers': centers.tolist(),
                                      'labels': labels.tolist(), 'samples': samples, 'keywords': keywords_total, 'metric': metric})
 
@@ -226,7 +230,7 @@ def getClusters():
     for idx, email in enumerate(emails):
         clusters[labels[idx]].append(email)
 
-    weight = "0.5"
+    weight = '0.5'
     # Create language models using srilm.
     for cluster in os.listdir(out):
         cluster_path = os.path.join(out, cluster)
@@ -239,76 +243,60 @@ def getClusters():
     return jsonify({'samples': samples, 'keywords': keywords_total, 'clusters': clusters})
 
 
-def error_detector(lmAdaptPath, sentence, threshold):
-    # Reading input language model.
-    models = arpa.loadf(lmAdaptPath)
-    # ARPA files may contain several models.
-    lm = models[0]
-    words = sentence.split()
-    scores = dict(zip(words, [0] * len(words)))
-    n_grams = list(ngrams(words, 3))
-    for n_gram in n_grams:
-        prop = lm.p(n_gram)
-        if prop < threshold:
-            for word in n_gram:
-                scores[word] += 1
-    sent_errors = ['0']
-    for n_gram in n_grams:
-        if scores[n_gram[1]] > 1:
-            sent_errors.append('1')
-        else:
-            sent_errors.append('0')
-    sent_errors.append('0')
-    return " ".join(sent_errors)
-
-
-@app.route("/dictation", methods=["POST"])
+@app.route('/dictation', methods=['POST'])
 def getDictation():
     '''Endpoint that decodes speech to text.
 
-        Args:
+        Body:
             cookie: Cookie of current user.
-            method: Use language adaptation or not.
-            package: Sphinx4 or pocketsphinx
-            url: Sound file.
+        Files:
+            url: Sound file to decode.
 
         '''
     cookie = request.form['cookie']
-    # method = request.form['method']
-    # package = request.form['package']
     url = request.files['url']
 
     # Get current user
     res = database.find_one('connections', {'_id': cookie})
     email_name = res['email_name']
 
-    out = os.path.join('./data', email_name)
     # Save current dictation in filesystem.
+    out = os.path.join('./data', email_name)
+    mllr_path = os.path.join(out, 'acoustic')
     url.save(os.path.join(out, 'curr_dictation.wav'))
 
+    # Speech to text using default models.
     decoded_gen = get_text_pocketsphinx(
         out, lmPath, acousticPath, dictPath, "")
+
+    # Classify output text to computed clusters.
     res = database.find_one('clusters', {'_id': email_name})
     centers = res['centers']
     metric = res['metric']
     doc = nlp(decoded_gen)
     decoded_gen_spacy = doc.vector
-    mllr_path = os.path.join(out, 'acoustic')
     cluster = closest_cluster(np.array(centers), decoded_gen_spacy, metric)
     clusterPath = os.path.join(out, 'clusters/cluster_' + str(cluster))
     lmAdaptPath = os.path.join(clusterPath, 'merged.lm')
+    # Speecg to text using adapted models.
     decoded_adapt = get_text_pocketsphinx(
         out, lmAdaptPath, acousticPath, dictPath, mllr_path)
 
+    # Compute possible error words.
     sent_errors = error_detector(lmAdaptPath, decoded_adapt, 0.05)
     returned_data = {'text_gen': decoded_gen,
                      'text_adapt': decoded_adapt, 'cluster': cluster, 'errors': sent_errors}
-
     return jsonify(returned_data)
 
 
-@app.route("/randomEmail", methods=["POST"])
+@app.route('/randomEmail', methods=['POST'])
 def get_random_email():
+    '''Endpoint that returns a random email sentence to the user.
+
+        Body:
+            cookie: Cookie of current user.
+        '''
+
     cookie = request.form['cookie']
     # Get current user
     res = database.find_one('connections', {'_id': cookie})
@@ -322,12 +310,13 @@ def get_random_email():
     return jsonify({'email': sel_sentence})
 
 
-@app.route("/saveDictation", methods=["POST"])
+@app.route('/saveDictation', methods=['POST'])
 def saveDictation():
     '''Endpoint that saves a sound file for later acoustic adaptation.
 
-        Args:
+        Body:
             cookie: Cookie of current user.
+        Files:
             url: Sound file.
 
         '''
@@ -345,6 +334,7 @@ def saveDictation():
     if not os.path.exists(wav_path):
         os.makedirs(wav_path)
 
+    # Keep the number of dictations in the database.
     res = database.find_one('savedDictations', {'_id': email_name})
     if res is None:
         counter = 0
@@ -355,25 +345,32 @@ def saveDictation():
         database.update_one('savedDictations', {'_id': email_name}, {
                             "$set": {'num': counter}})
 
+    # Keep a file that contains all the ids.
     with open(os.path.join(out, 'ids'), 'a') as f:
         f.write(str(counter) + '\n')
+    # Keep a file that contains all the transcriptions.
     with open(os.path.join(out, 'transcriptions'), 'a') as f:
         f.write('<s> ' + text.strip('\n') + ' </s> ' +
                 ' (' + str(counter) + ')' + '\n')
 
     # Save current dictation in filesystem.
     url.save(os.path.join(wav_path, str(counter) + '.wav'))
-
     return {'message': 'OK'}
 
 
-@app.route("/adaptAcoustic", methods=["POST"])
+@app.route('/adaptAcoustic', methods=['POST'])
 def adapt_acoustic():
+    '''Endpoint that adapts default acoustic model in user's previous dictations
+
+        Body:
+            cookie: Cookie of current user.
+        '''
     cookie = request.form['cookie']
     # Get current user
     res = database.find_one('connections', {'_id': cookie})
     email_name = res['email_name']
 
+    # Define necessary paths.
     out = os.path.join('./data', email_name)
     wav_path = os.path.join(out, 'wav')
     output = os.path.join(out, 'acoustic/')
@@ -383,10 +380,11 @@ def adapt_acoustic():
     feat_params = os.path.join(acousticPath, 'feat.params')
     mfc_path = os.path.join(output, 'mfc')
 
-    # Remove previous adaptation
+    # Remove previous adaptation if exists.
     if os.path.exists(output):
         shutil.rmtree(output)
 
+    # Generate counts.
     generate_command = 'sphinx_fe -argfile ' + feat_params + ' -samprate 16000 -c ' + \
         ids + ' -di ' + wav + ' -do ' + mfc_path + ' -ei wav -eo mfc -mswav yes'
     if subprocess.call([generate_command], shell=True):
@@ -416,16 +414,22 @@ def adapt_acoustic():
     return {'message': 'OK'}
 
 
-@app.route("/logOut", methods=["POST"])
+@app.route('/logOut', methods=['POST'])
 def log_out():
+    '''Endpoint that removes user's data.
+
+        Body:
+            cookie: Cookie of current user.
+
+        '''
     cookie = request.form['cookie']
     # Get current user
     res = database.find_one('connections', {'_id': cookie})
-    if res is None:
-        return jsonify({'message': 'error'})
     email_name = res['email_name']
     keep = res['keep']
     database.delete_one('connections', {'_id': cookie})
+
+    # If user don't want to keep his data, remove all of them.
     if not keep == "yes":
         database.delete_one('messages', {'_id': email_name})
         database.delete_one('clusters', {'_id': email_name})
